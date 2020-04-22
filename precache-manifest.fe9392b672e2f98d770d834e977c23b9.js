2d42c77d5ca89d925b09da2349411074fb824ae9
Hector Alcazar <hiad99@gmail.com>
2020-04-21 21:54:47 -0500

    Updates

---
+++ precache-manifest.fe9392b672e2f98d770d834e977c23b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164c283dcf14f97b1d9632cd1a2b7f44",
+    "url": "/homeBymodules/index.html"
+  },
+  {
+    "revision": "895241a03fcc2b0ca346",
+    "url": "/homeBymodules/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b6acfa931764e2d984d6",
+    "url": "/homeBymodules/static/js/2.09a7e466.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homeBymodules/static/js/2.09a7e466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895241a03fcc2b0ca346",
+    "url": "/homeBymodules/static/js/main.163a240f.chunk.js"
+  },
+  {
+    "revision": "a3f70a0e7bcf9f9e0e44",
+    "url": "/homeBymodules/static/js/runtime-main.411937bf.js"
+  }
+]);
